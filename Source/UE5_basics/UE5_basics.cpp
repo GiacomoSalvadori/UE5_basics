eb6e5a5199ed83bba0d0e45a7f882baab2f61665
Giacomo <giaco.salvadori@gmail.com>
2023-09-25 15:31:08 +0200

    add basics c++

---
+++ Source/UE5_basics/UE5_basics.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_basics.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_basics, "UE5_basics" );
+ 
